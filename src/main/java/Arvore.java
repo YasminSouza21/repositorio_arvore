c036790276fa5030d2ec8eb327af6dfc5707b116
Yasmin <yasmindesouza090@gmail.com>
2025-05-20 20:26:19 -0300

    add method count and insert

--- src/main/java/Arvore.java
+++ src/main/java/Arvore.java
@@ -9,12 +9,48 @@ public class Arvore {
         this.arvoreEsquerda = null;
     }
 
-    public Arvore(Integer valorNoRaiz){
+    public Arvore(Integer valorNoRaiz) {
         this.noRaiz = new No(valorNoRaiz);
         this.arvoreDireita = null;
         this.arvoreEsquerda = null;
     }
 
+    public void inserir(Integer valor) {
+        if (this.noRaiz == null) {
+            this.noRaiz = new No(valor);
+            System.out.println("add nó " + valor + " raiz");
+        } else if(valor < this.noRaiz.getValor()){
+            if (this.arvoreEsquerda == null) {
+                this.arvoreEsquerda = new Arvore(valor);
+                System.out.println("add no " + valor + " a esquerda do no " + this.noRaiz.getValor());
+            } else {
+                this.arvoreEsquerda.inserir(valor);
+            }
+        } else {
+            if(this.arvoreDireita == null){
+                this.arvoreDireita = new Arvore(valor);
+                System.out.println("add no " + valor + " a direita do no " + this.noRaiz.getValor());
+            } else {
+                this.arvoreDireita.inserir(valor);
+            }
+        }
 
+    }
 
+    public Integer count(){
+        if(this.noRaiz == null){
+            return  0;
+        }
+
+        int count = 1;
+        if(this.arvoreEsquerda != null){
+            count += this.arvoreEsquerda.count();
+        }
+
+        if(this.arvoreDireita != null){
+            count += this.arvoreDireita.count();
+        }
+
+        return count;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Arvore arvore = new Arvore();
 
+        arvore.inserir(4);
+        arvore.inserir(3);
+        arvore.inserir(5);
+        arvore.inserir(2);
+        arvore.inserir(6);
+        arvore.inserir(3);
+        arvore.inserir(7);
+        arvore.inserir(9);
+
+        System.out.println(arvore.count());
     }
 }
